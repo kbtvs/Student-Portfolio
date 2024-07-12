a78d7d5276b9f6ce2ad9fcf3ab476caed57d48c8
kbtvs <149474681+kbtvs@users.noreply.github.com>
2024-07-12 15:25:44 +0530

    Delete SRM SIP directory

--- SRM SIP/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('downloadBtn').addEventListener('click', function() {
-    const link = document.createElement('a');
-    link.href = 'resume_s.pdf';  // Link to the PDF file
-    link.download = 'Resume.pdf';
-    link.click();
-});
